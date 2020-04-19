a27aea017848e075b0ce98c09461037dd41ef5f0
Suji Kim <rlatnwl9595@naver.com>
2020-04-18 19:59:36 -0700

    Add extra credit feature 1: changing values inside of the variant

--- index.js
+++ index.js
@@ -2,19 +2,18 @@ addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
 
-const URL = 'https://cfw-takehome.developers.workers.dev/api/variants'
-
 /**
  * Respond with variants
  * @param {Request} resquest
  */
 async function handleRequest(request) {
+	const url = 'https://cfw-takehome.developers.workers.dev/api/variants'
 	const init = {
 		headers: {
 			'content-type':'application/json;charset=UTF-8'
 		}
 	}
-	let response = await fetch(URL, init)
+	let response = await fetch(url, init)
 		.then(function(response) {
 			if (!response.ok){
 				throw new Error(`HTTP error! status: ${response.status}`);
@@ -26,10 +25,7 @@ async function handleRequest(request) {
 	let index = selectRandomIndex(variants.length)
 
 	response = await fetch(variants[index], init)
-	return new HTMLRewriter().on('*', new ElementHandler()).transform(response)
-
-	// return new Response(variants[index], init)
-	// return new Response(variants, init)
+	return new HTMLRewriter().on('*', new ElementHandler(index)).transform(response)
 }
 
 function selectRandomIndex(maxNumber) {
@@ -37,15 +33,41 @@ function selectRandomIndex(maxNumber) {
 }
 
 class ElementHandler {
-  element(element) {
-    // An incoming element, such as `div`
-    console.log(`Incoming element: ${element.tagName}`)
-  }
-  comments(comment) {
-    // An incoming comment
-  }
+	constructor(index) {
+		this.variants_index = index
+	}
+	element(element) {
+		// An incoming element, such as `div`
+  		customizePage(element, this.variants_index)
+  	}
+}
 
-  text(text) {
-    // An incoming piece of text
-  }
+function customizePage(elementTag, variants_index) {
+	if (elementTag.tagName == "title"){
+		elementTag.setInnerContent("Su Ji's Cloudflare Internship Application")
+	}
+	else if (elementTag.tagName == "h1" && elementTag.getAttribute("id") == "title"){
+		elementTag.prepend("Su Ji's")
+		elementTag.append("App")
+	}
+	else if (elementTag.tagName == "p" && elementTag.getAttribute("id") == "description") {
+		if (variants_index == 0) {
+			elementTag.setInnerContent("Hello! This is my GitHub profile. You can also visit my personal website with the other variant.\
+				Thank you and stay safe!")
+		}
+		else {
+			elementTag.setInnerContent("Hello! This is my personal website. You can also visit my GitHub profile with the other variant.\
+				Thank you and stay safe!")
+		}
+	}
+	else if (elementTag.tagName == "a" && elementTag.getAttribute("id") == "url") {
+		if (variants_index == 0) {
+			elementTag.setAttribute("href", "https://github.com/SujiKim6")
+			elementTag.setInnerContent("Visit Su Ji's GitHub profile")
+		}
+		else {
+			elementTag.setAttribute("href", "https://sujikim6.github.io/")
+			elementTag.setInnerContent("Visit Su Ji's Personal website")
+		}
+	}
 }
